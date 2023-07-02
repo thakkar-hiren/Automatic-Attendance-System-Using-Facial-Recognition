0e222688cb153c317d1d1bef85daccbcb3491b6d
Hiren Thakkar <121098652+thakkar-hiren@users.noreply.github.com>
2023-07-02 20:14:28 +0530

    Update settings.py

--- FaceDemo/settings.py
+++ FaceDemo/settings.py
@@ -133,4 +133,4 @@ EMAIL_HOST_USER = 'gperiattendance@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = 'gperi##$$104'
+EMAIL_HOST_PASSWORD = 'password'
